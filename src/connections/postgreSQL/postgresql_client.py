11e16030f0ef24750d8c067e1604dc362a9f4e41
Alan <45725511+yykk7711@users.noreply.github.com>
2024-07-28 16:27:17 +0800

    global session added

--- src/connections/postgreSQL/postgresql_client.py
+++ src/connections/postgreSQL/postgresql_client.py
@@ -1,22 +1,16 @@
 from sqlalchemy.orm import Session
 from models.database_models import postgresql_schemas
 from models import postgresql_model
-from connections.postgreSQL.postgresql_connection import SessionLocal, engine
+from connections.postgreSQL.postgresql_connection import engine 
 from fastapi import Depends, FastAPI
+from connections.postgreSQL.postgresql_connection import global_sql_session
 
 class PostgresqlClient:
-    def __init__(self):
-        pass
-
-    def get_db(self):
-        db_session = SessionLocal()
-        try:
-            yield db_session
-        finally:
-            db_session.close()
+    def __init__(self, session: Session):
+        self.session = session
 
     # 1. Get user by user_id
-    def get_user(self, user_id: int, session: Session = Depends(get_db)):
-        return session.query(postgresql_model.Users).filter(postgresql_model.Users.user_id == user_id).first()
+    def get_user(self, user_id: int):
+        return self.session.query(postgresql_model.Users).filter(postgresql_model.Users.user_id == user_id).first()
 
-postgresqlClient = PostgresqlClient()
+postgresqlClient = PostgresqlClient(global_sql_session)
--- src/routers/postgresql.py
+++ src/routers/postgresql.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Session
 # from models.postgresql_model import Users
 from models.database_models.postgresql_schemas import User
 from connections.postgreSQL.postgresql_client import postgresqlClient
-from connections.postgreSQL.postgresql_connection import SessionLocal
 
 
 account_router = APIRouter(
@@ -11,16 +10,9 @@ account_router = APIRouter(
     tags=['Account']
 )
 
-def get_db():
-        db_session = SessionLocal()
-        try:
-            yield db_session
-        finally:
-            db_session.close()
-
 @account_router.get('/get_user', response_model=User)
-async def get_user(user_id: int, session: Session = Depends(get_db)) -> User:
-    db_user = postgresqlClient.get_user(user_id=user_id, session=session)
+async def get_user(user_id: int) -> User:
+    db_user = postgresqlClient.get_user(user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
